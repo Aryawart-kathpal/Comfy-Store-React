4fca15c830d7124a3b3a613abaace9c2d0f62962
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2024-07-06 13:19:34 +0530

    Deploy Changes

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const productionUrl= import.meta.env.PRODUCTION_URL || "http://localhost:5000/api/v1";
+const productionUrl= import.meta.env.VITE_PRODUCTION_URL || "http://localhost:5000/api/v1";
+// console.log(productionUrl);
 
 axios.defaults.withCredentials=true;
 export const customFetch = axios.create({
